bf832b96f25d6c6748a23c758b4802f36ad98cb4
Steven Shaw <steven@steshaw.org>
2021-06-29 22:01:44 +1000

    Controller

--- src/main/kotlin/org/steshaw/hellosb/HelloSbApplication.kt
+++ src/main/kotlin/org/steshaw/hellosb/HelloSbApplication.kt
@@ -2,10 +2,20 @@ package org.steshaw.hellosb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class HelloSbApplication
 
 fun main(args: Array<String>) {
-	runApplication<HelloSbApplication>(*args)
+    runApplication<HelloSbApplication>(*args)
+}
+
+@RestController
+class MyController {
+    @GetMapping("/")
+    fun root(): String {
+        return "ok\n"
+    }
 }
